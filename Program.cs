63741a3f5d81c5473d0ea5b8b9eced7f883f77ac
manunasci <nascimentomanuella73@gmail.com>
2021-09-17 21:48:42 -0300

    Adiciona o conversor de graus

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GrauCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a temperatura em graus °C para coverter: ");
+            string valordoGrau = Console.ReadLine();
+            double numerodoGrau = Double.Parse(valordoGrau);
+
+            double resultadodoGrau = (numerodoGrau*1.8)+32;
+
+            Console.WriteLine();
+
+            Console.WriteLine("°C = " + numerodoGrau);
+            Console.WriteLine(numerodoGrau + "°C equivale a " + resultadodoGrau + "°F");
+        }
+    }
+}
